7b7d17b70811ab9d3f66317a514e0924b4e43c56
Michael Dreesen <michaeldreesen90@gmail.com>
2020-10-13 23:53:25 -0600

    hashed passwords for POST and PUT routes

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {};
@@ -44,6 +45,17 @@ User.init({
         }
     }
 }, {
+    hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData
+        },
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+    },
     // Table configuration options go here (https://sequelize.org/v5/manual/models-definition.html#configuration)
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -74,6 +74,7 @@ router.put('/:id', (req, res) => {
     // SET username = "Lernantino", email = "lernantino@gmail.com", password = "newPassword1234"
     // WHERE id = 1;
     User.update(req.body, {
+            individualHooks: true,
             where: {
                 id: req.params.id
             }
