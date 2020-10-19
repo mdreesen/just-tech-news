df7ab64643d058745e7ac5d8ba547cd51c839e98
Michael Dreesen <michaeldreesen90@gmail.com>
2020-10-19 13:56:24 -0600

    added each post to html, loops through the html. Also added handlebars to the project

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+// adding the path to require the css
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// serves as static asset
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+// require handlebars
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+// connect to handlbars engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // force: false is this would drop all db tables on startup
 // turn on connection to db and server
