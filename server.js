e062e7a1265e56760352d9ca37b9741542ca1e94
Michael Dreesen <michaeldreesen90@gmail.com>
2020-10-19 16:59:34 -0600

    adding in sessions

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 // adding the path to require the css
 const path = require('path');
+// adding express session to see the users sessions
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,6 +21,20 @@ app.use(routes);
 // require handlebars
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+
+// adding the sess here for user sessions
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
+
+// putting the app.use below the const sess
+app.use(session(sess));
 
 // connect to handlbars engine
 app.engine('handlebars', hbs.engine);
