357507e342bfc6647a9f4ff4879b7010b9f1a691
Michael Dreesen <michaeldreesen90@gmail.com>
2020-10-21 12:49:36 -0600

    added if statements in the handlebars files for being logged in and out. Also added text box and upvote functionality to vote and comment on a post

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -36,7 +36,7 @@ router.get('/', (req, res) => {
         });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/post/:id', (req, res) => {
     Post.findOne({
             where: {
                 id: req.params.id
@@ -66,7 +66,12 @@ router.get('/:id', (req, res) => {
                 res.status(404).json({ message: 'No post found with this id' });
                 return;
             }
-            res.json(dbPostData);
+
+            // serialize the data
+            const post = dbPostData.get({ plain: true })
+
+            // pass data to template
+            res.render('single-post', { post })
         })
         .catch(err => {
             console.log(err);
@@ -89,13 +94,16 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
-    // custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote, Comment, User })
-        .then(updatedVoteData => res.json(updatedVoteData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    // make sure the session exists first
+    if (req.session) {
+        // pass session id along with all destructured properties on req.body
+        Post.upvote({...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+            .then(updatedVoteData => res.json(updatedVoteData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
 });
 
 router.put('/:id', (req, res) => {
