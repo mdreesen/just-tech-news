93b039483f3cd06033350d828afe7c029e5660c7
Michael Dreesen <michaeldreesen90@gmail.com>
2020-10-21 17:54:22 -0600

    made delete and put calls to update these routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const dashboardRoutes = require('./dashboard-routes')
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 // this is here because if we make a request to an endpoint that
 // does not exist, we'll receive a 404 error
---
+++ public/javascript/edit-post.js
@@ -0,0 +1,21 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (response.ok) {
+        document.location.replace('/dashboard')
+    } else {
+        alert(response.statusText)
+    }
+
+}
+
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler)
